2f93aaf41aebd90a2d26e476d40c71fc77fa703e
Behsse <behsse.pro@gmail.com>
2024-06-29 22:39:32 +0200

    Modification route getPosts

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -3,14 +3,32 @@
 import { prisma } from "@/lib/prisma";
 import { getCurrentUser } from "@/lib/session";
 
-export const getPosts = async () => {
+type User = {
+    id: string;
+    name: string | null;
+    email: string | null;
+    emailVerified: Date | null;
+    image: string | null;
+    admin: boolean;
+};
+
+type Post = {
+    id: number;
+    text: string;
+    completed: boolean;
+    createdAt: Date;
+    userId: string;
+    user: User;
+};
+
+export const getPosts = async (): Promise<Post[]> => {
     try {
-        const getPosts = await prisma.post.findMany({
+        const posts = await prisma.post.findMany({
             include: {
                 user: true
             }
         });
-        return getPosts;
+        return posts as Post[];
     } catch (error) {
         console.log(error);
         return [];
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,9 @@
-import { AuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { prisma } from "./prisma";
 
-export const authOptions : AuthOptions = {
+export const authOptions : NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
