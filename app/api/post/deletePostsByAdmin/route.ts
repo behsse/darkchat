ad6987ba913a86d2f925e4eeaa923c6383bd7ea0
Behsse <behsse.pro@gmail.com>
2024-10-12 23:58:59 +0200

    Change function DELETE from deletePostsByAdmin

--- app/api/post/deletePostsByAdmin/route.ts
+++ app/api/post/deletePostsByAdmin/route.ts
@@ -1,29 +1,30 @@
-"use server"
+"use server";
 
 import prisma from "@/lib/prisma";
 import { getCurrentUser } from "@/lib/session";
 import { NextResponse } from "next/server";
 
-export const DELETE = async (request : Request) => {
-    const user = await getCurrentUser()
+export const DELETE = async (request: Request) => {
+  const user = await getCurrentUser();
 
-    try {
-        const url = new URL(request.url);
-        const id = parseInt(url.searchParams.get("id") as string);
+  try {
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id"); // Assurez-vous d'utiliser base 10
 
-        if (!id || !user?.id || !user.admin) {
-            return NextResponse.json({ message: "Invalid data" }, { status: 400 });
-        }
-
-        const data = await prisma.post.delete({
-            where: {
-                id: id.toString(),
-            },
-        });
-        
-        return NextResponse.json({ message: "Post deleted successfully", data });
-    } catch (error) {
-        console.error("Error deleting post:", error);
-        return NextResponse.json({ message: "Failed to delete post" }, { status: 500 });
+    // Vérifiez si l'ID est valide et si l'utilisateur est un administrateur
+    if (!id || !user?.id || !user.admin) {
+      return NextResponse.json({ message: "Invalid data" }, { status: 400 });
     }
-}
+
+    const data = await prisma.post.delete({
+      where: {
+        id: id, // Utilisez l'ID directement
+      },
+    });
+
+    return NextResponse.json({ message: "Post deleted successfully", data });
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    return NextResponse.json({ message: "Failed to delete post" }, { status: 500 });
+  }
+};
